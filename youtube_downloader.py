f7d70d0610d2517ab773438f4770866b6a1e8ce1
TMCooper# <pokemon160377@gmail.com>
2024-08-20 17:16:00 +0200

    lil update code

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -9,40 +9,48 @@ PATH_VID = os.path.join(PATH, "Vidéo")
 PATH_PLAY = os.path.join(PATH, "Playlist")
 
 def main():
-    choix = input("Voulez-vous télécharger une playlist ou une vidéo ? (P/V) : ")
+    try:
+        print("Pour quiter le programme tapper Q")
+        choix = input("Voulez-vous télécharger une playlist ou une vidéo ? (P/V) : ").lower()
 
-    # Condition corrigée pour vérifier l'entrée utilisateur
-    while choix not in ["P", "p", "V", "v"]:
-        choix = input("Veuillez choisir entre une playlist et une vidéo (P/V) : ")
-    
-    if choix in ["V", "v"]:
-        if not os.path.exists(PATH_VID):
-            os.mkdir(PATH_VID)
-
-        Link = input("Entrez le lien de la vidéo : ")
-        yt = youtube_dl.YoutubeDL({'outtmpl': os.path.join(PATH_VID, '%(title)s.%(ext)s'),
-                                   'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
-                    })
-        ytv = yt.extract_info(Link, download=True)
-        print(f'{ytv["title"]} téléchargée avec succès vers {PATH_VID}')
-        subprocess.run("cls", shell=True)
+        # Condition corrigée pour vérifier l'entrée utilisateur
+        while choix not in ["p", "v", "q"]:
+            choix = input("Veuillez choisir entre une playlist et une vidéo (P/V) : ").lower()
         
-    elif choix in ["P", "p"]:
-        if not os.path.exists(PATH_PLAY):
-            os.mkdir(PATH_PLAY)
+        if choix in ["v"]:
+            if not os.path.exists(PATH_VID):
+                os.mkdir(PATH_VID)
 
-        Link = input("Entrez le lien de la playlist : ")
-        yt_play = Playlist(Link)
+            Link = input("Entrez le lien de la vidéo : ")
+            yt = youtube_dl.YoutubeDL({'outtmpl': os.path.join(PATH_VID, '%(title)s.%(ext)s'),
+                                    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
+                        })
+            ytv = yt.extract_info(Link, download=True)
+            print(f'{ytv["title"]} téléchargée avec succès vers {PATH_VID}')
+            subprocess.run("cls", shell=True)
+            
+        elif choix in ["p"]:
+            if not os.path.exists(PATH_PLAY):
+                os.mkdir(PATH_PLAY)
 
-        for i in yt_play.videos:
-            link_vid = i.watch_url
-            yt = youtube_dl.YoutubeDL({'outtmpl': os.path.join(PATH_PLAY, '%(title)s.%(ext)s'),
-                                       'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
-                    })
-            dl_vid = yt.extract_info(link_vid, download=True)
-            print(f'{dl_vid["title"]} téléchargée avec succès vers {PATH_PLAY}')
+            Link = input("Entrez le lien de la playlist : ")
+            yt_play = Playlist(Link)
 
-        subprocess.run("cls", shell=True)
+            for i in yt_play.videos:
+                link_vid = i.watch_url
+                yt = youtube_dl.YoutubeDL({'outtmpl': os.path.join(PATH_PLAY, '%(title)s.%(ext)s'),
+                                        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
+                        })
+                dl_vid = yt.extract_info(link_vid, download=True)
+                print(f'{dl_vid["title"]} téléchargée avec succès vers {PATH_PLAY}')
+
+            subprocess.run("cls", shell=True)
+        
+        elif choix in ["q"]:
+            exit
+
+    except KeyboardInterrupt:
+        print("\nL'utilisateur a saisit CTRL + C arret du programme ...")
 
 if __name__ == "__main__":
     main()
