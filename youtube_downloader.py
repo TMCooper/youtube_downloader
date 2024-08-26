5da5bc51ce4093fc240b933b450dd74e0c228815
TMCooper# <pokemon160377@gmail.com>
2024-08-26 20:59:00 +0200

    Update Code
    Update for translate and remove some line now useless

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -2,6 +2,7 @@ import yt_dlp as youtube_dl
 from pytubefix import Playlist
 import subprocess
 import os
+import json
 
 
 PATH = os.path.dirname(os.path.abspath(__file__))
@@ -10,36 +11,30 @@ PATH_PLAY = os.path.join(PATH, "Playlist")
 
 def main():
     try:
-        lang = input("What is your language ? (FR/EN) : ").lower()
+        valid_languages = ["en", "fr"]
 
-        while lang not in ["fr", "en"]:
-            lang = input("What is your language ? FR or EN").lower()
+        with open('languages.json', 'r') as lang_file:
+            languages = json.load(lang_file)
+
+        print(f"Available languages: {', '.join(valid_languages).upper()}")
+        lang = input("What is your language ? : ").lower()
+
+        while lang not in valid_languages:
+            print(f"Available languages: {', '.join(valid_languages).upper()}")
+            lang = input("Please select your language: ").lower()
         
-        if lang in ["en"]:
-            print("To exit the program type Q")
-            choix = input("Do you want to download a playlist or video? (P/V) : ").lower()
-
-        else:
-            print("Pour quiter le programme tapper Q")
-            choix = input("Voulez-vous télécharger une playlist ou une vidéo ? (P/V) : ").lower()
+        print(languages[lang]["Exit_q"])
+        choix = input(languages[lang]["User_request_P_V"]).lower()
 
         # Condition corrigée pour vérifier l'entrée utilisateur
         while choix not in ["p", "v", "q"]:
-            if lang not in ["fr", "en"]:
-                choix = input("Please choose between a playlist and a video (P/V) : ").lower()
-
-            else:
-                choix = input("Veuillez choisir entre une playlist et une vidéo (P/V) : ").lower()
+            choix = input(languages[lang]["Force_user_request_P_V"]).lower()
         
         if choix in ["v"]:
             if not os.path.exists(PATH_VID):
                 os.mkdir(PATH_VID)
 
-            if lang in ["en"]:
-                Link = input("Enter the video link : ")
-            
-            else:
-                print("Entrez le lien de la vidéo : ")
+            Link = input(languages[lang]["Requests_user_link"])
 
             ID_V = Link.split("watch?v=")[1].split("&")[0]
             video = (f'https://www.youtube.com/watch?v={ID_V}')
@@ -48,17 +43,15 @@ def main():
                                     'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
                         })
             ytv = yt.extract_info(video, download=True)
-            print(f'{ytv["title"]} téléchargée avec succès vers {PATH_VID}')
+            title = ytv["title"] 
+            print(languages[lang]["success_download"].format(title=title, path=PATH_VID))
             subprocess.run("cls", shell=True)
             
         elif choix in ["p"]:
             if not os.path.exists(PATH_PLAY):
                 os.mkdir(PATH_PLAY)
-            
-            if lang in ["en"]:
-                Link = input("Enter the playlist link:")
-            else:
-                Link = input("Entrez le lien de la playlist : ")
+
+            Link = input(languages[lang]["Requests_user_playlist"])
 
             yt_play = Playlist(Link)
 
@@ -68,26 +61,16 @@ def main():
                                         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
                         })
                 dl_vid = yt.extract_info(link_vid, download=True)
-                if lang in["en"]:
-                    print(f'{dl_vid["title"]} successfully download to {PATH_PLAY}')
-
-                else:
-                    print(f'{dl_vid["title"]} téléchargée avec succès vers {PATH_PLAY}')
-
+                title = dl_vid["title"]
+                print(languages[lang]["success_download"].format(title=title, path=PATH_PLAY))
             subprocess.run("cls", shell=True)
         
         elif choix in ["q"]:
-            if lang in ["en"]:
-                print("Exit...")
-            else:
-                print("Sortie...")
-            exit
+            print(languages[lang]["Exit_q"])
+            exit()
 
-    except KeyboardInterrupt:
-        if lang in ["en"]:
-            print("\nThe user typed CTRL + Stop the program...")
-        else:
-            print("\nL'utilisateur a saisit CTRL + C arret du programme ...")
+    except KeyboardInterrupt:       
+        print(languages[lang]["keyboard_interupt"])
 
 if __name__ == "__main__":
     main()
