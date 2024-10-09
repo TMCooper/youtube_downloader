f617a2cc0c6ac11125b20dd80c7e9453a7e1d98d
TMCooper# <pokemon160377@gmail.com>
2024-10-09 18:44:59 +0200

    Update Youtube_downloader.py
    new gestion of video playlist to bypass the bot detection

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -3,14 +3,24 @@ from pytubefix import Playlist
 import subprocess
 import os
 import json
-
+import sys
+import ctypes
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 PATH_VID = os.path.join(PATH, "Vidéo")
 PATH_PLAY = os.path.join(PATH, "Playlist")
+PROXY = 'http://218.155.31.188:8080'
 
 def main():
     try:
+        if is_admin():
+            # Si le script est lancé en tant qu'administrateur, continue l'exécution
+            set_proxy(PROXY)
+        else:
+            # Si le script n'est pas lancé en tant qu'administrateur, redémarre-le avec élévation
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+            return  # Sort de la fonction pour éviter d'exécuter le reste du code
+
         valid_languages = ["en", "fr"]
 
         with open('languages.json', 'r') as lang_file:
@@ -22,8 +32,7 @@ def main():
         while lang not in valid_languages:
             print(f"Available languages: {', '.join(valid_languages).upper()}")
             lang = input("Please select your language: ").lower()
-        
-        print(languages[lang]["Exit_q"])
+
         choix = input(languages[lang]["User_request_P_V"]).lower()
 
         # Condition corrigée pour vérifier l'entrée utilisateur
@@ -56,6 +65,7 @@ def main():
             yt_play = Playlist(Link)
 
             for i in yt_play.videos:
+                set_proxy('https://218.155.31.188:8080')
                 link_vid = i.watch_url
                 yt = youtube_dl.YoutubeDL({'outtmpl': os.path.join(PATH_PLAY, '%(title)s.%(ext)s'),
                                         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
@@ -68,9 +78,29 @@ def main():
         elif choix in ["q"]:
             print(languages[lang]["Exit_q"])
             exit()
-
-    except KeyboardInterrupt:       
+    
+    except KeyboardInterrupt:
         print(languages[lang]["keyboard_interupt"])
+    
+    finally:
+        reset_proxy()  # Réinitialise le proxy à la fin
+
+def set_proxy(proxy_url):
+    command = f'netsh winhttp set proxy proxy-server="{proxy_url}"'
+    subprocess.run(command, shell=True)
+
+def reset_proxy():
+    subprocess.run('netsh winhttp reset proxy', shell=True)
+
+def is_admin():
+    # Vérifie si le script est exécuté avec les droits administratifs
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 if __name__ == "__main__":
     main()
+    reset_proxy()  # Réinitialise le proxy après l'exécution principale
+    subprocess.run("cls", shell=True)
+
